8a29b3a629814588748b367575d51fb7101f9402
Doug <dougbacelar@gmail.com>
2019-06-23 13:50:13 +0100

    remove unused css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
-import './App.css';
 import ModalWrapper from './modals/ModalWrapper';
 import Header from './header/Header';
-import logo from '../assets/logo.svg';
 
 const App = () => (
   <div className='app'>
     <ModalWrapper />
     <Header />
-    <img src={logo} className='app-logo' alt='logo' />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
   </div>
 );
 
