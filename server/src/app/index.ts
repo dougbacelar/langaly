81d8dddb3cebbcfe259eeec2b7867a1a3e0f7374
Doug <dougbacelar@gmail.com>
2020-09-26 15:07:46 +0900

    tidy up

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,40 +1,13 @@
 import './env/setup';
-import './typeOverrides';
 import express from 'express';
-import cookie from 'cookie';
-import jwt from 'jsonwebtoken';
-import { hasStringProperty, isNonNullObject } from './typeGuards';
+import { validateUserMiddleware } from './middlewares/validateUser';
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-const validateCookieMiddleware: express.RequestHandler = (req, res, next) => {
-  const cookies = cookie.parse(req.headers.cookie || '');
-
-  try {
-    const jwtPayload =
-      cookies.langalyticket &&
-      jwt.verify(cookies.langalyticket, process.env.ACCESS_TOKEN_SECRET);
-
-    if (!isNonNullObject(jwtPayload)) {
-      throw new Error('Could not decode token properly');
-    }
-    if (
-      !hasStringProperty(jwtPayload, 'userId') ||
-      jwtPayload.userId.trim() === ''
-    ) {
-      throw new Error('Token is missing necessary information');
-    }
-    req.user = jwtPayload;
-  } catch (error) {
-    return res.redirect('/login');
-  }
-  next();
-};
-
-app.get('/loggedin', validateCookieMiddleware, (req, res) => {
+app.get('/loggedin', validateUserMiddleware, (req, res) => {
   res.send(`<h1>bro you in...</h1>`);
 });
 
